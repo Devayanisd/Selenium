961dc2af24badd85ecf66064f850c58ce3b9552c
Devayanisd <devayanisd96@gmail.com>
2025-06-06 23:08:41 +0530

    changes

--- TestSelJava/src/org/sample/BaseTest.java
+++ TestSelJava/src/org/sample/BaseTest.java
@@ -5,7 +5,7 @@ public class BaseTest {
 	public static void main(String[] args) {
 		
 		//System.out.println("Hello World 2025");
-		String input = "DEVAYANISURESHSIDDIYA";
+		String input = "DEVAYANISURESHSIDDIYA456";
 		input = input.toLowerCase();
 		System.out.println(input);
 		
